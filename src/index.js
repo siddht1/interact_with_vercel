6760afda76118a0b006d663cd056a6a7646e4e02
siddht1 <rt1yrru+github@gmail.com>
2023-06-19 18:39:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //check wheter kv is present in env variable or not
-KV_URL=process.env.KV_URL;
-KV_REST_API_TOKEN=process.env.KV_REST_API_TOKEN;
+const kv_url=process.env.KV_URL;
+const kv_token=process.env.KV_REST_API_TOKEN;
 
 // Enable CORS for specific origin
 app.use(cors({
