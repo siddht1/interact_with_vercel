1b8ff8b5e464499e8fcd9ee3b42364ccadc663f7
siddht1 <rt1yrru+github@gmail.com>
2023-06-19 18:19:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+import { kv } from "@vercel/kv";
+await kv.set("user_1_session", "session_token_value");
+const session = await kv.get("user_1_session");
+
 
 // Enable CORS for specific origin
 app.use(cors({
