01886d061d36b32291c25adbd147432f1de00567
siddht1 <rt1yrru+github@gmail.com>
2023-06-19 15:21:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { get } = require('@vercel/edge-config');
+const edge_config = require('@vercel/edge-config');
 
 
 const app = express();
